f2ad6b4a3947be8e72cab2954867b98f3e3334d4
ThatMoose <norieganicholas88@gmail.com>
2015-04-16 22:38:11 -0400

    User Prompt & Exponent Solution
    Basic user prompt; Used *= operator for exponent solution.

---
+++ OverlyNoriega_Nicholas_Expressions_Assignment/js/calculations.js
@@ -0,0 +1,14 @@
+
+var user_input = new Array(2);
+
+
+user_input[0] = prompt("Welcome to simple Kinetic Energy Calculator by Nico \n\n Please Enter Mass");
+
+
+user_input[1] = prompt("Please Enter Velocity");
+
+
+user_input[1] *= user_input[1];
+
+
+alert(user_input);
