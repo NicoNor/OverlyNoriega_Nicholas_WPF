4f76ef58c2e683c84f678d01f05ee8b9c929e8ca
ThatMoose <norieganicholas88@gmail.com>
2015-04-16 23:54:03 -0400

    Fixed wording and added solution
    The problem is now solved, but need to be prompted on screen (alert)
    and show in the log.

--- OverlyNoriega_Nicholas_Expressions_Assignment/js/calculations.js
+++ OverlyNoriega_Nicholas_Expressions_Assignment/js/calculations.js
@@ -1,14 +1,20 @@
-
+//
 var user_input = new Array(2);
+var mass,velocity;
+
+user_input[0] = prompt("Welcome to simple Kinetic Energy Calculator by Nico \n\n Please Enter Mass (number value)");
+
+//used Float for decimal precision
+mass = parseFloat(user_input[0]);
 
 
-user_input[0] = prompt("Welcome to simple Kinetic Energy Calculator by Nico \n\n Please Enter Mass");
+user_input[1] = prompt("Please Enter Velocity (number value");
+
+velocity = parseFloat(user_input[1])
+
+//assigning square value of Velocity
+velocity *= velocity;
 
 
-user_input[1] = prompt("Please Enter Velocity");
 
-
-user_input[1] *= user_input[1];
-
-
-alert(user_input);
+var KE = (mass * velocity) /2;
