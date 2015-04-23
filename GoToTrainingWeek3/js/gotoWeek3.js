7f8da4f37569a54f60f6cbfca8dbc466502be83b
ThatMoose <norieganicholas88@gmail.com>
2015-04-23 18:30:26 -0400

    Created Fruitbowl Array

---
+++ GoToTrainingWeek3/js/gotoWeek3.js
@@ -0,0 +1,31 @@
+/*
+Nicholas Noriega
+4/22/2015
+GoTO Week #3
+ */
+
+//alert("Testing to see if connected.")
+
+//Create an array of fruit names and then sort it
+
+//Array - variable that holds multiple variables
+//Index # of an Array always starts with 0!
+
+var bowlOfFruit = ["apple", "banana","pear","peace"];
+
+//See whole array
+console.log(bowlOfFruit);
+
+//Console.log the banana
+console.log(bowlOfFruit[2]);
+
+//How many items are in an array?
+//Length property - access this by using dot syntax
+//Fancy term for "use a period"
+console.log(bowlOfFruit.length);
+
+//Count the number of pears in our fruit bowl
+
+//Create a variable that will keep track of how many pears there are.
+var totalNumPears = 0;
+
