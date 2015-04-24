bc663f6a1a43564f0d1723751169f7f93f1200cc
ThatMoose <norieganicholas88@gmail.com>
2015-04-24 15:24:14 -0400

    Exponents now work. Added result string/prompt/console
    Added comments to explain new addition (console/alert/result)

--- OverlyNoriega_Nicholas_Conditionals_Assignment/js/conditionals.js
+++ OverlyNoriega_Nicholas_Conditionals_Assignment/js/conditionals.js
@@ -17,8 +17,14 @@ userInput = prompt("Please Enter Distance(r).");
 //Checking that input is a number, re-prompting suer for a number, and validating on console.
 isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):r=userInput;
 
-//Formula Solution. (Tested via calculator. Both JS and Calculator gave same answer when: 
-f = ((6.67384* pow(10,-11))*m1*m2) /pow(r,2);
+//Formula Solution. (Tested via calculator. Both JS and Calculator gave same answer when: m1(4), m2(4), r(4) = 6.67384e-11
+//The e in the result = Euler's number.
+f = ((6.67384* Math.pow(10,-11))*m1*m2) /Math.pow(r,2);
+
+//Result of formula is prompted on screen and on console.
+var result = String("The Gravitational Force is " + f);
+alert(result);
+console.log(result);
 
 //Conditional statements tell us what happens when F(force) =  a positive, negative, or is equal to zero.
 if(f < 0 ){
