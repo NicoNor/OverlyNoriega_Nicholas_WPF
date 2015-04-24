ed65cb796695d6d333e82a2a66d4196c4265aff9
ThatMoose <norieganicholas88@gmail.com>
2015-04-23 22:20:51 -0400

    Modified prompt and Added formula
    assigned proper variables to user prompts and added newtons formula

--- OverlyNoriega_Nicholas_Conditionals_Assignment/js/conditionals.js
+++ OverlyNoriega_Nicholas_Conditionals_Assignment/js/conditionals.js
@@ -7,13 +7,18 @@ var m1, m2, r, f, userInput;
 
 userInput = prompt("Welcome to Newton's Law of Universal Gravitation Calculator. \n\nPlease Enter Mass1(m1).");
 //Checking that input is a number, re-prompting suer for a number, and validating on console.
-isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):console.log("Is A Number");
+isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):m1=userInput;
 
 userInput = prompt("Please Enter Mass2(m2).");
 //Checking that input is a number, re-prompting suer for a number, and validating on console.
-isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):console.log("Is A Number");
+isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):m2=userInput;
 
 userInput = prompt("Please Enter Distance(r).");
 //Checking that input is a number, re-prompting suer for a number, and validating on console.
-isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):console.log("Is A Number");
+isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):r=userInput;
+
+//Formula is resolved.
+f = ((6.67384* pow(10,-11))*m1*m2) /pow(r,2);
+
+
 
