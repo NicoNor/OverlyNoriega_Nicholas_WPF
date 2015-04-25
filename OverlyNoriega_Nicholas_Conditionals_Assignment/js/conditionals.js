ce71b3581a89a950ca6a8446ee194b4542abc00c
ThatMoose <norieganicholas88@gmail.com>
2015-04-24 21:36:45 -0400

    Organized better
    Added spacing and comments that organize better

--- OverlyNoriega_Nicholas_Conditionals_Assignment/js/conditionals.js
+++ OverlyNoriega_Nicholas_Conditionals_Assignment/js/conditionals.js
@@ -7,30 +7,43 @@ var m1, m2, r, f, userInput;
 
 userInput = prompt("Welcome to Newton's Law of Universal Gravitation Calculator. \n\nPlease Enter Mass1(m1).");
 //Checking that input is a number, re-prompting user if number was not inputed.
-isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):m1=userInput;
+
 if(userInput === ""){
     //If the user left it blank
     //reprompt the user using the SAME variable
     userInput = prompt("Please Do Not Leave Blank. \nPlease Enter Mass1(m1)");
 }
 
+isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):m1=userInput;
+
+//*********************************
+
+
 userInput = prompt("Please Enter Mass2(m2).");
-//Checking that input is a number, re-prompting suer for a number, and validating on console.
-isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):m2=userInput;
+//Checking that input is a number, re-prompting user for a number, and validating on console.
+
 if(userInput === ""){
     //If the user left it blank
     //reprompt the user using the SAME variable
     userInput = prompt("Please Do Not Leave Blank. \nPlease Enter Mass2(m2)");
 }
 
+isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):m2=userInput;
+
+//**********************************
+
 userInput = prompt("Please Enter Distance(r).");
-//Checking that input is a number, re-prompting suer for a number, and validating on console.
-isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):r=userInput;
+
 if(userInput === ""){
     //If the user left it blank
     //reprompt the user using the SAME variable
     userInput = prompt("Please Do Not Leave Blank. \nPlease Enter Distance(r)");
 }
+
+isNaN(userInput)?userInput=prompt("Error: The Value You Have Entered Is Not A Number.\nPlease Enter A New Value."):r=userInput;
+
+//**********************************
+
 
 //Formula Solution. (Tested via calculator. Both JS and Calculator gave same answer when: m1(4), m2(4), r(4) = 6.67384e-11
 //The e in the result = Euler's number.
