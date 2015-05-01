0f9889d6764ae8a0b46b0c00fbf37330ba5670b9
ThatMoose <norieganicholas88@gmail.com>
2015-05-01 00:43:02 -0400

    Prompted for min number
    Validated for blank and not a number using a while loop

--- GoToTrainingWeek4/js/week4.js
+++ GoToTrainingWeek4/js/week4.js
@@ -5,3 +5,40 @@
 
 
 //alert("Testing to see if connected.");
+
+//Create a file that will give us a random number between two numbers
+
+//Ask the user for the min number
+var minNum = prompt("Let's find a number between two numbers!\nWhat is the min number:");
+
+// Validate the prompt is not blank and it is a number
+while(minNum==="" || isNaN(minNum)){
+    //re-prompt the user
+    if(minNum===""){
+        minNum = prompt("Please do not leave blank.\nWhat is the min number:");
+
+    } else if(isNaN(minNum)){
+
+        minNum = prompt("Please only use numbers.\n What is the min number:");
+
+    }
+
+    minNum = prompt("Please do not leave blank and only use numbers.\n What is the min number:");
+
+}
+
+/*
+Basic structure of a while loop
+
+//Declare a variable
+var x = prompt("Type in a number");
+
+while( x < 6 ){
+
+    //Some sort of change to the variable we in our condition to test.
+    x++;
+
+}
+
+ */
+
