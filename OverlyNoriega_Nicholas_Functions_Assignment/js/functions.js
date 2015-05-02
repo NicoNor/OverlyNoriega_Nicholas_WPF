3aeaad81c0234a774200d74088397809f431ae9c
ThatMoose <norieganicholas88@gmail.com>
2015-05-01 22:25:08 -0400

    Added prompt and 1 function
    Added prompts and 1 function(missing console logs to verify)

--- OverlyNoriega_Nicholas_Functions_Assignment/js/functions.js
+++ OverlyNoriega_Nicholas_Functions_Assignment/js/functions.js
@@ -4,5 +4,45 @@
 
 //alert("We are connected!")
 
+//We are going to calculate the price of an item with a discounted amount.
 
+// Prompting user to see if the calculator is useful to him.
+var question=prompt("Welcome to Price Finder.\n Do you need help finding a price AFTER discount?");
+
+//Making it so only yes and no can be used.
+while(question != "yes" && question != "no"){
+    question=prompt("Please only use yes or no.\n Are you happy today?");
+    //Making all answers lowercase to easily defind users answer.
+    question=question.toLocaleLowerCase()
+    if(question==="no")
+    alert("You do not need Price Finder.");
+}
+
+//We are now finding the original price.
+var ogPrice=prompt("What is the original price?");
+
+//Validate- directly after the prompt.
+while(ogPrice==="" || isNaN(ogPrice)) {
+    ogPrice = prompt("Please do not leave blank and only use numbers.\nWhat is the original price?");
+    conole.log(ogPrice);
+
+}
+
+//We are now finding the discount %
+var discount=prompt("What is the discount percent?");
+
+//Validating
+while(discount==="" || isNaN(discount)){
+    discount=prompt("Please do not leave blank and only use numbers.\nWhat is the discount percent?");
+}
+
+decimalizr(discount);
+
+//Converting user input for "discount" into decimal.
+
+function decimalizr(percent){
+    var discountPercent= percent / 100;
+        console.log(discountPercent);
+
+}
 
